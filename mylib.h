d1ecafa68d1631c88a594cf9bbe53b3bba0fc68c
Gentle Trader <ceschiatti@gmail.com>
2011-09-27 23:46:09 -0300

    Lib now has a Class

--- mylib.h
+++ mylib.h
@@ -2,3 +2,17 @@
 using namespace std;
 
 int soma(int a, int b);
+
+class MinhaClasse
+{
+	int a,b;
+public:
+	
+	MinhaClasse(int a = 0, int b = 0):a(a),b(b){};
+	
+	int multiplica()
+	{
+		return a*b;
+	}
+	
+};
